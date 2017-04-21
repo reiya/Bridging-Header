4e34f0da28cc4a99b70b76628c4acb5aeb6814af
Matsuki, Reiya (松木 玲也) <t-rmatsuki@valuecommerce.co.jp>
2017-04-21 14:56:56 +0900

    追加

---
+++ Bridging-Header/Classes/BHOCallSwift.m
@@ -0,0 +1,17 @@
+//
+//  BHOCallSwift.m
+//  Bridging-Header
+//
+//  Created by Matsuki, Reiya (松木 玲也) on 2017/04/21.
+//  Copyright © 2017年 Bridging-Header. All rights reserved.
+//
+
+#import "BHOCallSwift.h"
+
+@implementation BHOCallSwift
+
++(void)callSwift{
+    NSLog(@"No Setting Bridging Header Can Call Swift");
+}
+
+@end
