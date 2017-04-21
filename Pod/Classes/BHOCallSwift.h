c2fdf9bb574a982bc9cd2c795f745dbbb0273b3e
Matsuki, Reiya (松木 玲也) <t-rmatsuki@valuecommerce.co.jp>
2017-04-21 12:19:28 +0900

    cocoa pod

---
+++ Pod/Classes/BHOCallSwift.h
@@ -0,0 +1,13 @@
+//
+//  BHOCallSwift.h
+//  Bridging-Header
+//
+//  Created by Matsuki, Reiya (松木 玲也) on 2017/04/21.
+//  Copyright © 2017年 Bridging-Header. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BHOCallSwift : NSObject
++(void)callSwift;
+@end
